fbca821e7954ff9a24979f2e496f1621e0052de6
Om Kumar <om.kumar.-nd@disney.com>
2023-03-11 00:34:45 +0530

    added feign builder method

--- src/main/java/com/spring/template/config/FeignClientBeanConfig.java
+++ src/main/java/com/spring/template/config/FeignClientBeanConfig.java
@@ -1,8 +1,26 @@
 package com.spring.template.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
+
+import feign.Feign;
+import feign.Request;
+import feign.Retryer;
+import feign.Logger.Level;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.slf4j.Slf4jLogger;
 
 @Configuration
 public class FeignClientBeanConfig {
+
+	private <T> T getFeignBuilder(Class<T> type, int connectTimeoutMillis, int readTimeoutMillis, String uri) {
+		return Feign.builder().encoder(new JacksonEncoder()).decoder(new JacksonDecoder()).errorDecoder(null)
+				.logger(new Slf4jLogger()).retryer(new Retryer.Default()).logLevel(Level.FULL)
+				.options(new Request.Options(connectTimeoutMillis, TimeUnit.MILLISECONDS, readTimeoutMillis,
+						TimeUnit.MILLISECONDS, true))
+				.target(type, uri);
+	}
 
 }
